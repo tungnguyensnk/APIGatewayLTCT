5955ee722d4a422ca6c70e2b4687b714b507995e
tungnguyensnk <tung.nd194714@sis.hust.edu.vn>
2023-01-12 18:10:37 +0700

    init

---
+++ src/test/java/com/apigatewayltct/ApiGatewayLtctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigatewayltct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayLtctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
